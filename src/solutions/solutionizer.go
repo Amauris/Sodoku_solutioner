cdba5b19f53d7008ed7271f09b77e41298dcd5d2
Amauris <amauris@amauris-Ideapad-V460.(none)>
2015-11-12 22:09:28 -0500

    Removing unused code

--- src/solutions/solutionizer.go
+++ src/solutions/solutionizer.go
@@ -7,13 +7,6 @@ import (
 )
 
 type Solutionizer struct {
-	familyDensityOrdered []family
-}
-
-type family struct {
-	members []int
-	locX int
-	locY int
 }
 
 //main entry function that calls the right
@@ -25,63 +18,6 @@ func (inst *Solutionizer) GetSodokuAnswer(board *sodoku.Board) string {
 	}
 
 	return board.GetStringFormat()
-}
-
-func (inst *Solutionizer) isNumberTaken(availableNumbers, nth int) bool {
-
-	//assumes nth start from 0
-	//check if nth position of 987654321 is zero
-	//by dividing 987654321 by 10^nth and retrieving remainder
-	//then with remainder you divide by 10^(n-1) in order to retrieve
-	//nth digit
-	nthFloat := float64(nth)
-	remainder := availableNumbers%int(math.Pow(10, nthFloat))
-
-	denominator := int(math.Pow(10, nthFloat-1))
-
-	if(denominator==0) {
-		return false
-	}
-
-	nthNumber := remainder/denominator
-	
-	isTaken := (nth!=nthNumber)
-
-	return isTaken
-}
-
-func (inst *Solutionizer) availableNumbers(availableNumbers int, family []int) int {
-
-	for _, v := range(family) {
-
-		if(v==0 || inst.isNumberTaken(availableNumbers, v)) {
-			continue
-		}
-
-		reducer := v*int(math.Pow(10, float64(v-1)))
-		availableNumbers -= reducer
-	}
-
-	return availableNumbers
-}
-
-//retrieved numbders from availableNumbers and converts into array of numbers
-func (inst *Solutionizer) getPossibilitiesFromAvailableNumbers(availableNumbers int) []int {
-
-	possibilities := []int{}
-	//first get the maxinum nth number available
-	max := int(math.Log10(float64(availableNumbers))) + 1
-
-	for max > 0 {
-
-		if(!inst.isNumberTaken(availableNumbers, max)) {
-			possibilities = append(possibilities, max)
-		}
-
-		max -= 1
-	}
-
-	return possibilities
 }
 
 //retrieves the family with the least
@@ -142,4 +78,61 @@ func (inst *Solutionizer) SetIndexWithLeastPossibleChoices(board *sodoku.Board) 
 	}
 
 	return true
+}
+
+//retrieved numbders from availableNumbers and converts into array of numbers
+func (inst *Solutionizer) getPossibilitiesFromAvailableNumbers(availableNumbers int) []int {
+
+	possibilities := []int{}
+	//first get the maxinum nth number available
+	max := int(math.Log10(float64(availableNumbers))) + 1
+
+	for max > 0 {
+
+		if(!inst.isNumberTaken(availableNumbers, max)) {
+			possibilities = append(possibilities, max)
+		}
+
+		max -= 1
+	}
+
+	return possibilities
+}
+
+func (inst *Solutionizer) availableNumbers(availableNumbers int, family []int) int {
+
+	for _, v := range(family) {
+
+		if(v==0 || inst.isNumberTaken(availableNumbers, v)) {
+			continue
+		}
+
+		reducer := v*int(math.Pow(10, float64(v-1)))
+		availableNumbers -= reducer
+	}
+
+	return availableNumbers
+}
+
+func (inst *Solutionizer) isNumberTaken(availableNumbers, nth int) bool {
+
+	//assumes nth start from 0
+	//check if nth position of 987654321 is zero
+	//by dividing 987654321 by 10^nth and retrieving remainder
+	//then with remainder you divide by 10^(n-1) in order to retrieve
+	//nth digit
+	nthFloat := float64(nth)
+	remainder := availableNumbers%int(math.Pow(10, nthFloat))
+
+	denominator := int(math.Pow(10, nthFloat-1))
+
+	if(denominator==0) {
+		return false
+	}
+
+	nthNumber := remainder/denominator
+	
+	isTaken := (nth!=nthNumber)
+
+	return isTaken
 }
